f34b2358d56312d745a8c74901a28c875a5e3a2b
1 <1@1->
2023-04-04 20:50:23 +0300

    Добавьте файлы проекта.

---
+++ Laba1_4/BoolArr.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1_4
+{
+    internal class BoolArr
+    {
+        private bool[] arr;
+        private bool res;
+        public bool exception = false;
+        public int counter = 0;
+        // конструктор класу
+        public BoolArr(int sizeOfArr)
+        {
+            arr = new bool[sizeOfArr];
+        }
+        // оголошення індексатору
+        public bool this[int i]
+        {
+            get {
+
+                if (i >= 0 && i < arr.Length)
+                {
+                    return !arr[i];
+                }
+                else
+                {
+                    exception = true;
+                    return false;
+                }
+            }
+            set
+            {
+                if (i >= 0 && i < arr.Length)
+                {
+                    arr[i] = value;
+                }
+            }
+        }
+        // властивість
+        public bool Result
+        {
+            get
+            { 
+                for(int i = 0; i<arr.Length; i++)
+                {
+                    if (arr[i]==false)
+                    {
+                        counter++;
+                    }
+                }
+                if(counter==0)
+                {
+                    res = false;
+                    return res;
+                }
+                if(counter>=1)
+                {
+                    res = true;
+                    return res;
+                }
+                else
+                {
+                    res = false;
+                    return res;
+                }
+            }
+            set
+            {
+                value = res;
+            }
+        }
+    }
+}
---
+++ Laba1_4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Markup;
+using Laba1_4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введiть розмiр: ");
+        string sizeStr;
+        sizeStr = Console.ReadLine();
+        int SizeInt = Convert.ToInt32(sizeStr);
+        BoolArr arr = new BoolArr(SizeInt);
+        int counter = 0;
+        Console.WriteLine("Введiть значення булевого типу(true/false)");
+        Console.WriteLine("Для закiнчення введення напишiть stop");
+        for(int i = 0; i<999;)
+        {
+            Console.Write(i+")");
+            string value;
+            value = Console.ReadLine();
+            if (value == "true")
+            {
+                arr[i] = true;
+                i++;
+                counter++;
+            }
+            else if (value == "false")
+            {
+                arr[i] = false;
+                i++;
+                counter++;
+            }
+            else if (value == "stop")
+            {
+                break;
+            }
+            else Console.WriteLine("значення повиннi бути лише true/false");
+        }
+        Console.WriteLine("______Повернення оберненого значення______");
+        for(int i = 0; i<counter;i++)
+        {
+            Console.Write(i+")"+arr[i]);
+            if (arr.exception)
+            {
+                Console.Write(" - поза межами");
+            }
+            Console.WriteLine("");
+        }
+        Console.WriteLine("______Результат логiчного додавання______");
+        for (int i = 0; i < SizeInt; i++)
+        {
+            if (i == SizeInt - 1)
+            {
+                Console.Write(arr[i] + " = ");
+            }
+            else
+            {
+                Console.Write(arr[i] + " || ");
+            }
+        }
+        bool result = arr.Result;
+        Console.WriteLine(result);
+    }
+}
